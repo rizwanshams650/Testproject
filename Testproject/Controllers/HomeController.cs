b316b1663b443d7bb489440856b871e023d2e973
rizwanshams650 <shamsrizwan650@gmail.com>
2025-04-08 17:33:25 -0400

    implimented parametrize logging for actions.

--- Testproject/Controllers/HomeController.cs
+++ Testproject/Controllers/HomeController.cs
@@ -6,11 +6,11 @@ namespace Testproject.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger;
 
-        public HomeController()
+        public HomeController(ILogger<HomeController> logger)
         {
-            //_logger = logger;
+            _logger = logger;
         }
 
         public IActionResult Index()
@@ -32,18 +32,24 @@ namespace Testproject.Controllers
         [HttpGet]
         public IActionResult EnterValue()
         {
+            _logger.LogInformation("Get EnterValue at {Time}", DateTime.UtcNow);
+
             return View();
         }
         [HttpPost]
         public IActionResult EnterValue(UserInput input)
         {
+            _logger.LogInformation("Post EnterValue called with user input:{Value} at {Time}", input.Value, DateTime.UtcNow);
             TempData["UserValue"] = input.Value;
             return RedirectToAction("DisplayValue");
         }
 
         public IActionResult DisplayValue()
         {
-            ViewBag.UserValue = TempData["UserValue"];
+            var value = TempData["UserValue"];
+            ViewBag.UserValue = value;
+            _logger.LogInformation("DisplayValue called with TempData value: {Value}", value);
+
             return View();
         }
 
