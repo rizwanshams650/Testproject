8764e624d7062a27988aba17188a9a968b4e0d52
rizwanshams650 <shamsrizwan650@gmail.com>
2025-04-08 15:50:20 -0400

    Add project files.

---
+++ Testproject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Testproject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
